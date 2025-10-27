32dbc09d7db914b8f7446d8b6583af0e48e46574
ferreiravaldir <fortesvaldir@gmail.com>
2025-10-27 11:26:01 -0100

    aws terraform

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,22 +1,22 @@
 variable "tf_state_bucket" {
-    description = "Name of S3 bucket in AWS for stoing TF state"
-    default = "devops-recipe-app-bu"
-  
+  description = "Name of S3 bucket in AWS for stoing TF state"
+  default     = "devops-recipe-app-bu"
+
 }
 
 
 variable "tf_state_lock_table" {
   description = "Name of the DynamoDB table for TF state locking"
-  default = "devops-recipe-app-api-tf-lock"
+  default     = "devops-recipe-app-api-tf-lock"
 }
 
 variable "project" {
-    description = "Project name for tagging resources"
-    default = "recipe-app-api"
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
 
-  }
+}
 
 variable "contact" {
   description = "Contact name for tagging resources"
-  default = "ferreiravaldir1999@gmail.com"
+  default     = "ferreiravaldir1999@gmail.com"
 }
